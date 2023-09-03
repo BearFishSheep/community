3512e0f36f534486322b9656f91140a730947d04
xiongyuyang <xiongyuyang0909@163.com>
2023-09-03 09:34:27 +0800

    更新邮件发送、注册功能

--- src/main/java/com/nocoder/community/service/UserService.java
+++ src/main/java/com/nocoder/community/service/UserService.java
@@ -2,6 +2,12 @@ package com.nocoder.community.service;
 
 import com.nocoder.community.entity.User;
 
+import java.util.Map;
+
 public interface UserService {
     User findById(int id);
+
+    Map<String, Object> register(User user);
+
+    int activation(int userId, String code);
 }
