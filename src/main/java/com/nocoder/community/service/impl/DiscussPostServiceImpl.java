0881bdde8c2e2b45ad491a29379f9a4d480068be
xiongyuyang <xiongyuyang0909@163.com>
2023-09-07 21:24:02 +0800

    更新发布帖子、查看帖子详情功能

--- src/main/java/com/nocoder/community/service/impl/DiscussPostServiceImpl.java
+++ src/main/java/com/nocoder/community/service/impl/DiscussPostServiceImpl.java
@@ -3,8 +3,10 @@ package com.nocoder.community.service.impl;
 import com.nocoder.community.dao.DiscussPostMapper;
 import com.nocoder.community.entity.DiscussPost;
 import com.nocoder.community.service.DiscussPostService;
+import com.nocoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -12,6 +14,9 @@ import java.util.List;
 public class DiscussPostServiceImpl implements DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
 
     @Override
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
@@ -21,5 +26,27 @@ public class DiscussPostServiceImpl implements DiscussPostService {
     @Override
     public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+    @Override
+    public int addDiscussPost(DiscussPost post) {
+        if (post == null) {
+            throw new IllegalArgumentException("参数不能为空！");
+        }
+
+        // 转义HTML标记
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+
+        // 过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
+    }
+
+    @Override
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
     }
 }
