898e3b5502af5e956ead09983bd87f82b974b052
xiongyuyang <xiongyuyang0909@163.com>
2023-09-19 16:59:22 +0800

    加入统一记录日志功能，使用了AOP - 小修

--- src/main/java/com/nocoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nocoder/community/aspect/ServiceLogAspect.java
@@ -1,6 +1,5 @@
 package com.nocoder.community.aspect;
 
-import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -8,10 +7,8 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.servlet.mvc.condition.RequestConditionHolder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
